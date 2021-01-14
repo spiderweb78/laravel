f42e633c2c1420788a6f1cb23bc1ce94739738d7
spiderweb78 <hlaingminoo7777@gmail.com>
2021-01-14 21:25:00 +0630

    added admin routes and home view

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
+--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -20,3 +21,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// -Admin route 
+Route::get('admin/home', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.home');
+Route::get('admin/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('admin.login');
+Route::post('admin/login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('admin.login');
+Route::post('admin/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('admin.logout');
+Route::get('admin/password/confirm', [App\Http\Controllers\Auth\ConfirmPasswordController::class, 'showConfirmForm'])->name('admin.password.confirm');
+Route::post('admin/password/confirm', [App\Http\Controllers\Auth\ConfirmPasswordController::class, 'confirm']);
+Route::post('admin/password/email', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('admin.password.email');
+Route::get('admin/password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('admin.password.request');
+Route::post('admin/password/reset', [App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('admin.password.update');
+Route::get('admin/password/reset/{token}', [App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('admin.password.reset');
+Route::get('admin/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('admin.register');
+Route::post('admin/register', [App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('admin.register');
